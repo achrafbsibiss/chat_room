186911ed5a16e1e90c40efd012d43a32e8bf27d4
achrafbsibiss <achraf.bsibiss11@gmail.com>
2024-08-07 13:50:55 +0200

    Add the reset input for the chat

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,5 @@
 class RoomsController < ApplicationController
-  before_action :set_room, only: %i[update destroy show]
+  before_action :set_room, only: %i[show]
 
   def index
     @room = Room.new
@@ -21,17 +21,6 @@ class RoomsController < ApplicationController
   def create
     @room = Room.create(set_params)
   end
-
-  def update
-    if @room.update(set_params)
-      flash.now[:notice] = 'succes'
-      redirect_to rooms_path
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  def destroy; end
 
   private
 
