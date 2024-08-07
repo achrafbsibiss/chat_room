81871da92cf15fb3093b6d98e1e0e39086725d08
achrafbsibiss <achraf.bsibiss11@gmail.com>
2024-08-07 20:36:51 +0200

    Add the image for the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,19 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :messages
+  has_one_attached :avatar
   has_many :participants, dependent: :destroy
 
   scope :all_except, ->(user) { where.not(id: user) }
   after_create_commit { broadcast_append_to 'users' }
+
+  def avatar_url_of_default
+    if avatar.attached?
+      Rails.application.routes.url_helpers.rails_blob_url(
+        avatar, only_path: true
+      )
+    else
+      'users/user.png'
+    end
+  end
 end
