30e3b27a23c75d26996f204793b290a887a90c15
achrafbsibiss <achraf.bsibiss11@gmail.com>
2024-08-08 14:16:28 +0200

    add the icons for the online

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,5 @@
 module UsersHelper
+  def connect_user(online)
+    online ? 'inline-block w-4 h-4 mr-2 bg-green-400 rounded-full' : 'inline-block w-4 h-4 mr-2 bg-gray-400 rounded-full' # Use 'bg-gray-400' for offline status, not 'bg-current'
+  end
 end
